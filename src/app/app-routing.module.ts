4b909fd612883505d1ed1616e81cf6ad51fa48dc
DESKTOP-8B2DGJ1\Timon Bestebreur <timonbestebreur@gmail.com>
2018-05-28 21:27:12 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about/:id/:name',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
